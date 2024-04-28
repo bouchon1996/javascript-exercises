85e8fde67be43e70499294a3b7ebda781b835200
bouchon1996 <bouchon1996@gmail.com>
2024-04-27 19:40:36 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = [] 
+    for (let i=0; i < string.length; i++){
+        stringArray.push(string[i]);
+    }
+    let reversedArray = stringArray.reverse();
+    return reversedArray.join(''); 
 };
 
 // Do not edit below this line
