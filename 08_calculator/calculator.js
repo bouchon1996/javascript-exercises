f1e68a4a98bd33c184e2aeb7bc9779f17c364881
bouchon1996 <bouchon1996@gmail.com>
2024-06-10 13:55:16 -0500

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+	let addition = a + b;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+	let subtraction = a - b;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array){
+  let multiplication = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiplication = multiplication * array[i];
+  }
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let answer = 0;
+  answer = a ** b;
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let answer = 1;
+  for (let i = 1; i <= a; i++) {
+    answer = answer * i
+  }
+  return answer;
 };
 
 // Do not edit below this line
